c6a220de14e2500bd5d1c5ab34ac0f0fa5dd1be9
MG-Botz <113174711+MgBotz@users.noreply.github.com>
2022-10-01 12:43:00 +0700

    config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6282171432358']
+global.ownernomer = "6282171432358"
+global.premium = ['6282171432358']
+global.packname = 'Sticker By MGBOTZ'
+global.author = 'MGBOTZ'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
